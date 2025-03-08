0842f449c91058a44ca9545f92ad472716fa3bb8
Roy0902 <97725342+Roy0902@users.noreply.github.com>
2025-03-08 16:46:44 +0800

    version-1.0.1
    version-1.0.1

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 import express from 'express';
-import routes from './routes/index.js';
+import routes from './route/index.js';
 
 const app = express();
 
--- config/database.js
+++ config/database.js
@@ -1,32 +1,11 @@
-import {Connector} from '@google-cloud/cloud-sql-connector';
-import {Pool} from 'mysql2';
+import {createPool} from 'mysql2';
 import 'dotenv/config';
 
-const connector = new Connector();
-
-const clientOpts = await connector.getOptions({
-    instanceConnectionName: process.env.INSTANCE_CONNECTION_NAME, 
-    authType: 'IAM', 
-});
-
-const pool = new Pool({
-            ...clientOpts,
-            user: process.env.DB_USER, 
-            password: process.env.DB_PASSWORD, 
-            database: process.env.DB_NAME, 
-});
-
-pool.query('SELECT NOW() AS now', (err, result) => {
-  if (err) {
-      console.error('Error connecting to the database:', err);
-  } else {
-      console.log('Connected to the database:', result.rows[0]);
-  }
-});
-
-process.on('SIGINT', () => {
-  pool.end();
-  connector.close();
+const pool = createPool({
+  host: process.env.DB_HOST, 
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
 });
 
 export default pool;
